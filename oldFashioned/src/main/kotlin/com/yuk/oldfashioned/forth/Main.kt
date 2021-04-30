b6ff80d7c79110dc55dc545df1fb0536ecc60b64
yukyongsoo <yozx234@gmail.com>
2021-05-01 02:59:18 +0900

    wip

--- oldFashioned/src/main/kotlin/com/yuk/oldfashioned/forth/Main.kt
+++ oldFashioned/src/main/kotlin/com/yuk/oldfashioned/forth/Main.kt
@@ -1,4 +1,33 @@
 package com.yuk.oldfashioned.forth
 
+import java.io.File
+import java.util.*
+
 class Main {
+    // 연산의 대상이 되는 데이터 스택
+    private val stack = Stack<String>()
+    //연산 해야 할 데이터를 저장하는 힙 메모리
+    private val heap = hashMapOf<String,List<String>>()
+
+    fun run() {
+        readFile()
+        filterOnlyChars()
+        scan()
+
+    }
+
+    private fun readFile() {
+        stack.push("text")
+        stack.push(File(stack.pop()).readText())
+    }
+
+    private fun filterOnlyChars() {
+        stack.push(stack.pop().replace("\r\n"," "))
+        stack.push(stack.pop().replace(","," "))
+        stack.push(stack.pop().replace("."," "))
+    }
+
+    private fun scan() {
+
+    }
 }
--- oldFashioned/src/test/kotlin/com/yuk/oldfashioned/ForthTest.kt
+++ oldFashioned/src/test/kotlin/com/yuk/oldfashioned/ForthTest.kt
@@ -1,4 +1,11 @@
 package com.yuk.oldfashioned
 
+import com.yuk.oldfashioned.forth.Main
+import org.junit.jupiter.api.Test
+
 class ForthTest {
+    @Test
+    fun test() {
+        Main().run()
+    }
 }
