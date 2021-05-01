97ce963c64854649169d516629d5553691691007
yukyongsoo <yozx234@gmail.com>
2021-05-02 04:11:18 +0900

    complete forth
    add word for memory

--- oldFashioned/src/main/kotlin/com/yuk/oldfashioned/forth/Main.kt
+++ oldFashioned/src/main/kotlin/com/yuk/oldfashioned/forth/Main.kt
@@ -5,29 +5,54 @@ import java.util.*
 
 class Main {
     // 연산의 대상이 되는 데이터 스택
-    private val stack = Stack<String>()
+    private val wordStack = Stack<String>()
+    // 코틀린에서는 스택을 Any로 해야 하므로 사실은 하나의 스택이나 두개로 분리한다
+    private val numberStack = Stack<Int>()
+
     //연산 해야 할 데이터를 저장하는 힙 메모리
-    private val heap = hashMapOf<String,List<String>>()
+    private val heap = hashMapOf<String,Int>()
 
     fun run() {
         readFile()
         filterOnlyChars()
-        scan()
-
+        splitWord()
+        countingWord()
+        printWord()
     }
 
     private fun readFile() {
-        stack.push("text")
-        stack.push(File(stack.pop()).readText())
+        wordStack.push(ClassLoader.getSystemClassLoader().getResource("text").file)
+        wordStack.push(File(wordStack.pop()).readText())
     }
 
     private fun filterOnlyChars() {
-        stack.push(stack.pop().replace("\r\n"," "))
-        stack.push(stack.pop().replace(","," "))
-        stack.push(stack.pop().replace("."," "))
+        wordStack.push(wordStack.pop().replace("\r\n"," "))
+        wordStack.push(wordStack.pop().replace(","," "))
+        wordStack.push(wordStack.pop().replace("."," "))
     }
 
-    private fun scan() {
+    private fun splitWord() {
+        wordStack.pop().split(" ").forEach {
+            wordStack.push(it)
+        }
+    }
 
+    private fun countingWord() {
+        while (wordStack.isNotEmpty()) {
+            numberStack.push(heap[wordStack.peek()] ?: 0)
+            heap[wordStack.pop()] = numberStack.pop() + 1
+        }
+    }
+
+    private fun printWord() {
+        heap.forEach { (word, frequency) ->
+            wordStack.push("$word : $frequency")
+        }
+
+        wordStack.forEach {
+            println(it)
+        }
+
+        wordStack.clear()
     }
 }
--- oldFashioned/src/main/kotlin/com/yuk/oldfashioned/memory/Main.kt
+++ oldFashioned/src/main/kotlin/com/yuk/oldfashioned/memory/Main.kt
@@ -2,6 +2,8 @@ package com.yuk.oldfashioned.memory
 
 import java.io.FileNotFoundException
 
+// 사실은 MainClass가 없어야 한다.
+// 추가적으로 memory와 dataNode는 시스템 자원이라고 생각해야 한다
 class Main {
     // 읽는 메모리 제한
     private val memory = ByteArray(1024)
@@ -9,6 +11,7 @@ class Main {
     // 데이터의 빈도수 저장하는 공간. 보조기억 장치로 가정한다. 나중에 변경예정
     private val dataNode = hashMapOf<String, Int>()
 
+    // 아래의 함수도 사실은 함수 블록만 main 함수라 여겨야 한다. 즉 다른 함수 자체가 존재할 수 없다
     fun run() {
         val file = ClassLoader.getSystemClassLoader().getResourceAsStream("text")
             ?: throw FileNotFoundException()
