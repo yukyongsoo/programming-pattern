55a6505fcb4785b9e4d208b724ad631f874b184d
yukyongsoo <yozx234@gmail.com>
2021-05-04 01:47:25 +0900

    complete codegolf

--- basic/src/main/kotlin/com/yuk/basic/codegolf/Main.kt
+++ basic/src/main/kotlin/com/yuk/basic/codegolf/Main.kt
@@ -1,4 +1,13 @@
 package com.yuk.basic.codegolf
 
+import java.io.File
+
 class Main {
+    fun run() {
+        val filePath = ClassLoader.getSystemClassLoader().getResource("text").file
+        File(filePath).forEachLine {
+            val word = it.replace("\r\n", " ").replace(",", " ").replace(".", " ").split(" ")
+            println(word.groupBy { it }.mapValues { it.value.size })
+        }
+    }
 }
---
+++ basic/src/test/kotlin/com/yuk/basic/CodeGolfTest.kt
@@ -0,0 +1,11 @@
+package com.yuk.basic
+
+import com.yuk.basic.codegolf.Main
+import org.junit.jupiter.api.Test
+
+class CodeGolfTest {
+    @Test
+    fun test() {
+        Main().run()
+    }
+}
