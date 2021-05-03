1acc6a7eea41bc62ead4a1898bf70db76b0283f0
yukyongsoo <yozx234@gmail.com>
2021-05-04 01:12:12 +0900

    complete pipeline

--- basic/src/main/kotlin/com/yuk/basic/pipeline/Main.kt
+++ basic/src/main/kotlin/com/yuk/basic/pipeline/Main.kt
@@ -1,4 +1,37 @@
 package com.yuk.basic.pipeline
 
+import java.io.File
+
 class Main {
+    fun run() {
+        printFrequency(countWord(splitLine(removeSpecialChar(readFile("text")))))
+    }
+
+    private fun printFrequency(result: Map<String, Int>) {
+        result.forEach { (word, number) ->
+            println("$word : $number")
+        }
+    }
+
+    private fun countWord(words: List<String>): Map<String, Int> {
+        return words.groupBy { it }.mapValues { it.value.count() }
+    }
+
+    private fun splitLine(lines: List<String>): List<String> {
+        return lines.flatMap {
+            it.split(" ")
+        }
+    }
+
+    private fun removeSpecialChar(lines: List<String>): List<String> {
+        return lines.map {
+            it.replace("\r\n", " ").replace(",", " ").replace(".", " ")
+        }
+    }
+
+    private fun readFile(fileName: String): List<String> {
+        val filePath = ClassLoader.getSystemClassLoader().getResource(fileName).file
+        val file = File(filePath)
+        return file.readLines()
+    }
 }
---
+++ basic/src/test/kotlin/com/yuk/basic/PipelineTest.kt
@@ -0,0 +1,11 @@
+package com.yuk.basic
+
+import com.yuk.basic.pipeline.Main
+import org.junit.jupiter.api.Test
+
+class PipelineTest {
+    @Test
+    fun test() {
+        Main().run()
+    }
+}
