ce75c7398ffbba84f24086881afc53f86a359c2a
yukyongsoo <yozx234@gmail.com>
2021-05-11 02:01:38 +0900

    complete infinite mirror

--- functionCompositon/src/main/kotlin/com/yuk/function/infinitemirror/Main.kt
+++ functionCompositon/src/main/kotlin/com/yuk/function/infinitemirror/Main.kt
@@ -1,4 +1,47 @@
 package com.yuk.function.infinitemirror
 
+import java.io.File
+
 class Main {
+    private val frequency = hashMapOf<String, Int>()
+
+    fun run() {
+        val words = getWords(readFile())
+        count(words)
+        print(0)
+    }
+
+    private fun readFile(): File {
+        val filePath = ClassLoader.getSystemClassLoader().getResource("text").file
+        return File(filePath)
+    }
+
+    private fun getWords(file: File): MutableList<String> {
+        val wordList = mutableListOf<String>()
+
+        file.forEachLine { line ->
+            val filteredLine = line.replace("\r\n", " ").replace(",", " ").replace(".", " ")
+            val words = filteredLine.split(" ")
+            wordList.addAll(words)
+        }
+
+        return wordList
+    }
+
+    private tailrec fun count(words: MutableCollection<String>) {
+        if (words.isEmpty())
+            return
+        val word = words.first()
+        words.remove(word)
+        frequency[word] = frequency.getOrDefault(word, 0) + 1
+        count(words)
+    }
+
+    private fun print(index: Int) {
+        if (frequency.size == index)
+            return
+        frequency.onEachIndexed { current, entry ->
+            println("${entry.key} : ${entry.value}")
+        }
+    }
 }
--- functionCompositon/src/test/kotlin/com/yuk/function/IMTest.kt
+++ functionCompositon/src/test/kotlin/com/yuk/function/IMTest.kt
@@ -1,4 +1,12 @@
 package com.yuk.function
 
+import com.yuk.function.infinitemirror.Main
+import org.junit.jupiter.api.Test
+
 class IMTest {
+    @Test
+    fun run(){
+        Main().run()
+    }
+
 }
