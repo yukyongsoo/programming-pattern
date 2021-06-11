a20bc592c840e6b94c71c6bfe677578ebd3e37e0
yukyongsoo <yozx234@gmail.com>
2021-06-12 03:30:17 +0900

    add dual map reduce but not complete

---
+++ concurrent/src/test/kotlin/com/yuk/concurrent/DualMapReduceTest.kt
@@ -0,0 +1,11 @@
+package com.yuk.concurrent
+
+import com.yuk.concurrent.dualmapreduce.Main
+import org.junit.jupiter.api.Test
+
+class DualMapReduceTest {
+    @Test
+    fun test() {
+        Main().run()
+    }
+}
